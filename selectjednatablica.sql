2f1f888b3b5ff4b91e42c3809b8185abe3ec5fcf
zagiter <tomislavkucic@gmail.com>
2024-01-17 10:58:42 +0100

    Select i spajanje

---
+++ selectjednatablica.sql
@@ -0,0 +1,107 @@
+﻿select * from smjerovi;
+
+-- filtriranje kolona ide između select i from
+-- * označava sve kolone
+-- naziv kolone - pojedine kolone
+select naziv, naziv from smjerovi;
+
+select naziv, cijena, * from smjerovi;
+
+-- koloni se može promijeniti naziv
+select naziv as smjer_naziv, cijena from smjerovi;
+
+-- konstanta - uvijek ista vrijendost
+
+select naziv, cijena, 'Osijek' as mjesto, 2 as broj from smjerovi;
+
+-- izraz - funkcija
+select naziv, len(naziv) as znakova from smjerovi;
+
+-- najjednostavniji select
+select getdate();
+
+-- filtriranje redova
+-- operatori uspoređivanja
+-- =
+select ime, prezime from polaznici
+where prezime='Tolić';
+
+-- uspoređivanja < > <= >=
+select * from polaznici where sifra<12;
+
+-- ostali operatori
+-- like -> % označava bilo koji znak(ove)
+-- Svi polaznici koji u svom imenu imaju slovo a
+select * from polaznici
+where ime like '%a%';
+
+-- between
+select * from polaznici
+where sifra between 2 and 5;
+
+-- in
+select * from polaznici
+where sifra in (2,7,9);
+
+-- logički operatori
+-- and (logički i)
+select * from polaznici
+where ime like '%a%' and sifra>10;
+
+-- not (logičko ne)
+select * from polaznici
+where ime not like '%a%';
+
+-- primjeri like
+-- svi polaznici čije ime počinje slovom T
+select * from polaznici
+where ime like 'm%';
+
+-- polaznice - s najmanjom reškom ime završava na a
+select * from polaznici
+where ime like '%a';
+
+-- or (logičko ili)
+select * from polaznici
+where ime like 'm%' or ime like 'l%';
+
+-- slaganje rezultata
+-- order by
+-- po prezimenu uzlazno (A->Ž)
+select * from polaznici
+where ime like 'm%' or ime like 'l%'
+order by prezime; --asc se i ne mora pisati
+
+-- po prezimenu silazno (Ž->A)
+select * from polaznici
+where ime like 'm%' or ime like 'l%'
+order by prezime desc;
+
+-- limitiranje podataka
+select top 10 * from polaznici;
+
+-- kreiranje nove tablice na osnovu select podataka
+select top 10 * 
+into odabrani from polaznici;
+
+select * from odabrani;
+
+drop table odabrani;
+
+-- Baza knjižnica
+use knjiznica;
+
+-- Marka je ostavila cura. On sada pati!
+-- Dolazi u knjižnicu kod Vas i pita što mu
+-- preporučujete da čita
+
+select * from katalog 
+where naslov like '%ljubav%'
+and sifra not in (2784,2938,3063,2541);
+
+-- Koja mjesta su pobjednici u igri kaladont?
+select * from mjesto
+where naziv like '%nt';
+
+-- koliko ima mjesta
+select count(*) from mjesto;
---
+++ spajanjetablica.sql
@@ -0,0 +1,115 @@
+﻿
+-- baza edunovawp3
+
+select * from smjerovi where sifra=1;
+
+select a.naziv as smjer, b.naziv as grupa
+from smjerovi a inner join grupe b
+on b.smjer = a.sifra;
+
+-- alternative
+select smjerovi.naziv as smjer, grupe.naziv as grupa
+from smjerovi inner join grupe
+on grupe.smjer = smjerovi.sifra;
+
+select a.naziv as smjer, b.naziv as grupa
+from smjerovi a, grupe b
+where a.sifra=b.smjer;
+
+-- ispisati naziv grupe te ime i prezime voditelja
+select a.naziv, concat(b.prezime, ' ', b.ime) as predavac
+from grupe a inner join predavaci b
+on a.predavac=b.sifra;
+
+select * from grupe;
+
+select a.naziv as smjer,
+b.naziv as grupa,
+concat(c.ime, ' ', c.prezime) as predavac,
+concat(e.ime,' ',e.prezime) as polaznik
+from smjerovi a 
+inner join grupe b on a.sifra=b.smjer
+inner join predavaci c on b.predavac=c.sifra
+inner join clanovi d on d.grupa=b.sifra
+inner join polaznici e on d.polaznik=e.sifra;
+
+-- recite da Šaki vodi WP2
+
+select * from predavaci;
+select * from grupe where predavac=2;
+
+update grupe set predavac=2 where sifra=2;
+
+select a.naziv as smjer, b.naziv as grupa
+from smjerovi a left join grupe b
+on a.sifra=b.smjer
+order by 2 ;
+
+-- baza knjiznica
+
+use knjiznica;
+-- Koje je sve knjige objavio August Šenoa
+select a.naslov
+from katalog a inner join autor b
+on a.autor=b.sifra
+where b.ime='August' and b.prezime='Šenoa';
+
+-- DZ:
+-- Izlistajte sve naslove knjiga
+-- koje su izdane u Osijeku
+
+-- Koliko izdavača su Društva s ograničenom odgovornošću?
+select  naziv from izdavac
+where naziv like '%d.o.o.%'
+or naziv like '%doo%'
+or naziv like '%d.o.o%';
+
+-- unesite sebe kao autora
+insert into autor (sifra,ime,prezime,datumrodenja) values
+(4,'Tomislav','Jakopec','1980-12-07');
+
+select top 10 * from autor;
+
+-- koji su autori rođeni u vašoj godini rođenja?
+select * from autor where
+datumrodenja between '1980-01-01' and '1980-12-31';
+
+
+-- Ispišite ime i prezime autora koji su 
+-- izdali knjige u Zagrebu
+select a.ime, a.prezime
+from autor a inner join katalog b on a.sifra=b.autor
+inner join mjesto c on b.mjesto=c.sifra
+where c.naziv='Zagreb';
+
+-- koji autori nisu izdali niti jednu knjigu?
+select a.ime, a.prezime, b.naslov
+from autor a left join katalog b on a.sifra=b.autor
+where b.naslov is null;
+
+-- obriši ih
+
+delete a
+from autor a left join katalog b on a.sifra=b.autor
+where b.naslov is null;
+
+-- svim autorima koji su 
+-- objavili knjigu u mjestu koje ima slovo a
+-- imenu dodajte Plemeniti. 
+-- Npr ako se zvao Mirko, sada će se zvati Plemeniti Mirko
+select * from autor;
+select concat('Plemeniti ',a.ime), c.naziv
+from autor a inner join katalog b on a.sifra=b.autor
+inner join mjesto c on b.mjesto=c.sifra
+where c.naziv like '%a%';
+
+update a 
+set a.ime=concat('Plemeniti ',a.ime)
+from autor a
+inner join katalog b on a.sifra=b.autor
+inner join mjesto c on b.mjesto=c.sifra
+where c.naziv like '%a%';
+
+
+-- koliko ima autora u bazi
+select count(*) from autor;
