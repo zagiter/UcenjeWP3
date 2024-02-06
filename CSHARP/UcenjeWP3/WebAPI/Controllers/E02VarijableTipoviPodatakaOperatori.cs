a13fe016c6fcc4d4dd7f398b71ce469b9d48399b
zagiter <tomislavkucic@gmail.com>
2024-02-06 22:08:17 +0100

    Create E02VarijableTipoviPodatakaOperatori.cs

---
+++ CSHARP/UcenjeWP3/WebAPI/Controllers/E02VarijableTipoviPodatakaOperatori.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("E02")]
+    public class E02VarijableTipoviPodatakaOperatori:ControllerBase
+    {
+
+        // ovdje počinje ruta
+        [HttpGet]
+        [Route("zad1")]
+        public int Zad1(int prvi, int drugi)
+        {
+            return prvi + drugi;
+        }
+        // ovdje završava ruta
+        
+
+    }
+}
