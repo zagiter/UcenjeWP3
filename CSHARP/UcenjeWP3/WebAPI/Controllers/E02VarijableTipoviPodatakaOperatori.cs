1d1550e823a73ff2523de77d8da68d912ad62a8f
zagiter <tomislavkucic@gmail.com>
2024-02-06 23:37:05 +0100

    Update E02VarijableTipoviPodatakaOperatori.cs

--- CSHARP/UcenjeWP3/WebAPI/Controllers/E02VarijableTipoviPodatakaOperatori.cs
+++ CSHARP/UcenjeWP3/WebAPI/Controllers/E02VarijableTipoviPodatakaOperatori.cs
@@ -4,18 +4,46 @@ namespace WebAPI.Controllers
 {
     [ApiController]
     [Route("E02")]
-    public class E02VarijableTipoviPodatakaOperatori:ControllerBase
+    public class E02VarijableTipoviPodatakaOperatori : ControllerBase
     {
 
         // ovdje počinje ruta
         [HttpGet]
         [Route("zad1")]
-        public int Zad1(int prvi, int drugi)
+        public int Zad1()
         {
-            return prvi + drugi;
+            return int.MaxValue;
         }
         // ovdje završava ruta
-        
 
+
+
+
+        // ovdje počinje ruta
+        [HttpGet]
+        [Route("zad2")]
+        public float Zad2(int a, int b)
+        {
+            // ruta prima kvocijent dvaju primljenih brojeva
+            return a / (float)b;
+        }
+        // ovdje završava ruta zz
+
+        [HttpGet]
+        [Route("zad3")]
+        public float Zad3(int a, int b)
+        {
+
+            return (a * b) + (a / (float)b);
+        }
+
+        [HttpGet]
+        [Route("zad5")]
+        public string Zad5(string a, string b)
+        {
+
+            return a + " " + b;
+
+        }
     }
 }
