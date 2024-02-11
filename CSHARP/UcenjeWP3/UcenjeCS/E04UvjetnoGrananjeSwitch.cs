db302f723e6d0dd680e14527c0d2c3cf61161e9b
zagiter <tomislavkucic@gmail.com>
2024-02-11 23:14:24 +0100

    Update E04UvjetnoGrananjeSwitch.cs

--- CSHARP/UcenjeWP3/UcenjeCS/E04UvjetnoGrananjeSwitch.cs
+++ CSHARP/UcenjeWP3/UcenjeCS/E04UvjetnoGrananjeSwitch.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,59 @@ namespace UcenjeCS
 {
     internal class E04UvjetnoGrananjeSwitch
     {
+        public static void Izvedi() 
+        {
+            int i = 1;
+
+            //Ako je i = 0 napiši DOBAR, i=1 napiši LOŠ, i=2 napiši ZAO
+
+            if (i == 0)
+            {
+                Console.WriteLine("DOBAR");
+            }
+            else if (i == 1)
+            {
+                Console.WriteLine("LOŠ");
+            }
+            else if (i == 2) 
+            {
+                Console.WriteLine("ZAO");
+            }
+
+            switch (i) 
+            {
+                case 0:
+                    Console.WriteLine("DOBAR");
+                    break;
+                case 1:
+                    Console.WriteLine("LOŠ");
+                    break; 
+                case 2:
+                    Console.WriteLine("ZAO");
+                    break;
+                default: // ovo je else u if-u
+                    Console.WriteLine("OSTALO");
+                    break;
+            }
+
+            Console.Write("Unesi ime grada:  ");
+            string grad = Console.ReadLine();
+
+            switch (grad)
+            {
+                case "Osijek":
+                case "Vukovar":
+                    Console.WriteLine("Slavonija");
+                    break;
+                case "Split":
+                case "Zadar":
+                    Console.WriteLine("Dalmacija");
+                    break;
+
+
+
+            }
+        
+        }
     }
 }
